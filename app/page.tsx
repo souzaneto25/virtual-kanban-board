66fd23740703893d75a4ae21575b8d5fe7058033
Souza Neto <souzaneto25@hotmail.com>
2024-12-20 11:23:26 -0300

    fix: adjusting to build pass

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-import KanbanBoard from "@/components/KanbanBoard";
+"use client";
+import dynamic from "next/dynamic";
+
+const KanbanBoard = dynamic(() => import("@/components/KanbanBoard"), {
+  ssr: false,
+});
 
 export default function Home() {
   return <KanbanBoard />;
